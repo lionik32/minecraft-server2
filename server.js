2c8b08e170fe2dd90874b46c3a8efd5e23aed02d
lionik32 <lionikelias@gmail.com>
2026-05-16 12:54:51 -0500

    Add sneaking and sprinting properties to player data

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ setInterval(() => {
         const lista = [];
         for (let [senderId, sender] of players.entries()) {
             if (senderId !== receiverId && sender.salaId === receiver.salaId && sender.ws.readyState === 1) {
-                lista.push({ id: senderId, x: sender.x, y: sender.y, z: sender.z, yaw: sender.yaw, pitch: sender.pitch || 0, moving: sender.moving || false, placing: sender.placing || false });
+                lista.push({ id: senderId, x: sender.x, y: sender.y, z: sender.z, yaw: sender.yaw, pitch: sender.pitch || 0, moving: sender.moving || false, placing: sender.placing || false, sneaking: sender.sneaking || false, sneakOffset: sender.sneakOffset || 0, sprinting: sender.sprinting || false });
             }
         }
         if (receiver.ws.readyState === 1) {
